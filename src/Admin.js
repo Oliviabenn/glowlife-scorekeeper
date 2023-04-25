0bc0695e8c7ddf842c0f59ae60f8fc4938fefa63
Olivia <oliviabennington@gmail.com>
2023-04-25 00:33:06 -0400

    styling, home form

---
+++ src/Admin.js
@@ -0,0 +1,7 @@
+/* import { Admin, Resource } from 'react-admin';
+
+export const Admin = () => (
+    <Admin basename="/admin" dataProvider={...}>
+        <Resource name="scores" {...scores} />
+    </Admin>
+); */
